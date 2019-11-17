17ab6f7f80727f16ea0aea5f095c5ca2510bf47f
Kamil <53897136+KamilJ90@users.noreply.github.com>
2019-11-17 20:43:06 +0100

    Stage 2 completed

--- src/machine/CoffeeMachine.java
+++ src/machine/CoffeeMachine.java
@@ -1,13 +1,30 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
+    private static final int ML_OF_WATER_REQUIRED_FOR_ONE_CUP = 200;
+    private static final int ML_OF_MILK_REQUIRED_FOR_ONE_CUP = 50;
+    private static final int G_OF_COFFEE_BEANS_REQUIRED_FOR_ONE_CUP = 15;
+
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee\n" +
-                "Grinding coffee beans\n" +
-                "Boiling water\n" +
-                "Mixing boiled water with crushed coffee beans\n" +
-                "Pouring coffee into the cup\n" +
-                "Pouring some milk into the cup\n" +
-                "Coffee is ready!");
+        Scanner scanner = new Scanner(System.in);
+        int cupsOfCoffee;
+        int mlOfWater;
+        int mlOfMilk;
+        int gOfCoffeeBeans;
+
+        System.out.println("Write how many cups of coffee you will need: ");
+        cupsOfCoffee = scanner.nextInt();
+        // One cup of coffee needs: 200 ml of water, 50 ml of milk, 15 g of coffee beans
+
+        mlOfWater = cupsOfCoffee * ML_OF_WATER_REQUIRED_FOR_ONE_CUP;
+        mlOfMilk = cupsOfCoffee * ML_OF_MILK_REQUIRED_FOR_ONE_CUP;
+        gOfCoffeeBeans = cupsOfCoffee * G_OF_COFFEE_BEANS_REQUIRED_FOR_ONE_CUP;
+
+        System.out.printf("For %d cups of coffee you will need:\n" +
+                "%d ml of water\n" +
+                "%d ml of milk\n" +
+                "%d g of coffee beans\n", cupsOfCoffee, mlOfWater, mlOfMilk, gOfCoffeeBeans);
     }
 }
